8805b305a6cfc55f0e08261562da7d0f43b159e4
Seixas <eduardo.seixas@zetti.tech>
2025-05-29 17:42:16 -0300

    Commit funcional no vb6 chamando

---
+++ ITratarBoleto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ApiBancos
+{
+   [ComVisible(true)]
+   [InterfaceType(ComInterfaceType.InterfaceIsDual)]
+   [Guid("9ABC47F7-AD42-4B4E-8243-B7FC96F0B96B")]
+   public interface ITratarBoleto
+   {
+      void SaveToFile(string filePath, string text);
+   }
+
+
+
+
+
+}
---
+++ TratarBoleto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace ApiBancos
+{
+   [ComVisible(true)]
+   [ClassInterface(ClassInterfaceType.None)]
+   [Guid("4CF02CB0-9504-46C5-95DC-2D539AD3FFAE")]
+   public class TratarBoleto : ITratarBoleto
+   {
+      public void SaveToFile(string filePath, string text)
+      {
+         string directoryPath = Path.GetDirectoryName(filePath);
+         if (!Directory.Exists(directoryPath))
+         {
+            Directory.CreateDirectory(directoryPath);
+         }
+         File.WriteAllText(filePath, text);
+      }
+
+
+
+
+
+    }
+}
